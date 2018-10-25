84cbe35d2e7b9661319fb450694099c770b9a656
Kristóf Marussy <kris7topher@gmail.com>
2018-10-25 14:34:00 +0200

    Demonstrate Gradle Kotlin version clash

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,16 +8,16 @@ dependencies {
     compile(kotlin("gradle-plugin", "1.2.71"))
 }
 
-configurations.all {
-    val isKotlinCompiler = name == "embeddedKotlin" || name.startsWith("kotlin") || name.startsWith("kapt")
-    if (!isKotlinCompiler) {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin" && requested.module.name == "kotlin-compiler-embeddable") {
-                useVersion("1.2.71")
-            }
-        }
-    }
-}
+//configurations.all {
+//    val isKotlinCompiler = name == "embeddedKotlin" || name.startsWith("kotlin") || name.startsWith("kapt")
+//    if (!isKotlinCompiler) {
+//        resolutionStrategy.eachDependency {
+//            if (requested.group == "org.jetbrains.kotlin" && requested.module.name == "kotlin-compiler-embeddable") {
+//                useVersion("1.2.71")
+//            }
+//        }
+//    }
+//}
 
 repositories {
     gradlePluginPortal()
